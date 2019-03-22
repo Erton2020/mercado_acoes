d53aab677bb6feb0b19cd8a711f5035eb5f426d0
Cleber Amaral <cleber.amaral@accenture.com>
2019-03-22 19:20:36 -0300

    Adequação de configuração do swagger

--- src/main/java/br/pucminas/stockmarket/api/config/SwaggerConfig.java
+++ src/main/java/br/pucminas/stockmarket/api/config/SwaggerConfig.java
@@ -11,18 +11,20 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+@Configuration
+@EnableSwagger2
 public class SwaggerConfig {
 	  @Bean
 	    public Docket api() {
 			return new Docket(DocumentationType.SWAGGER_2).useDefaultResponseMessages(false).select()
-					.apis(RequestHandlerSelectors.basePackage("br.pucminas.gerenciadorfavoritos.api.controllers"))
+					.apis(RequestHandlerSelectors.basePackage("br.pucminas.stockmarket.api.controllers"))
 					.paths(PathSelectors.any()).build()
 					.apiInfo(apiInfo());
 		}
 
 		private ApiInfo apiInfo() {
-			return new ApiInfoBuilder().title("Gerenciador de Favoritos API")
-					.description("Documentação da API de acesso aos recursos do Gerenciador de Favoritos.").version("1.0")
+			return new ApiInfoBuilder().title("Mercado de Ações API")
+					.description("Documentação da API de acesso aos recursos do Mercador de Ações.").version("1.0")
 					.build();
 		}
 }
--- src/main/java/br/pucminas/stockmarket/api/controllers/StockController.java
+++ src/main/java/br/pucminas/stockmarket/api/controllers/StockController.java
@@ -2,10 +2,6 @@ package br.pucminas.stockmarket.api.controllers;
 
 import java.util.List;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,13 +9,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.pucminas.stockmarket.api.dto.StockDTO;
-import br.pucminas.stockmarket.api.entities.Stock;
 import br.pucminas.stockmarket.api.services.StockService;
 import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiParam;
-import io.swagger.annotations.ApiResponse;
-import io.swagger.annotations.ApiResponses;
 import io.swagger.annotations.SwaggerDefinition;
 import io.swagger.annotations.Tag;
 
@@ -40,7 +31,7 @@ public class StockController {
 	}
 
 	@GetMapping(value = "/stocks/{companyId}", produces = "application/json")
-	public List<StockDTO> findStocksByCompany(@RequestParam(value = "Identificador da empresa para consulta de sações disponiveis.", required= false) Long companyId)
+	public List<StockDTO> findStocksByCompany(@RequestParam(value = "Identificador da empresa para consulta de ações que estão disponiveis para a venda.", required= false) Long companyId)
 	{
 		return stockService.findStocksByCompanyId(companyId);
 	}
